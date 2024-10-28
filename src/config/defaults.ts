e38c46ca15c14d3fcf87bad8a7c6d882dc3580c9
Julio Canizalez <juliocanizalez@outlook.com>
2024-10-28 16:31:44 -0600

    Add defaults module

---
+++ src/config/defaults.ts
@@ -0,0 +1,2 @@
+export const DEFAULT_NO_IMAGE_URL =
+  "https://png.pngtree.com/png-vector/20221125/ourmid/pngtree-no-image-available-icon-flatvector-illustration-pic-design-profile-vector-png-image_40966566.jpg";

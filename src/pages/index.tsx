2e115b5cadf6290a80832d07b07fba23acfc2ce1
Julio Canizalez <juliocanizalez@outlook.com>
2024-10-28 16:38:16 -0600

    Update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { title, subtitle } from "@/components/primitives";
+import { title, subtitle } from "@/components/common/primitives";
 import DefaultLayout from "@/layouts/default";
 
 export default function IndexPage() {
